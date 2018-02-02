8e705e2dff284379f559705972cafb750cf0b412
JACK12SPARROW <35995686+JACK12SPARROW@users.noreply.github.com>
2018-02-02 13:27:22 +0530

    tarun.c

---
+++ tarunaravind.C
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int number;
+
+    printf("Enter an integer: ");
+    scanf("%d", &numbER;
+    
+    if(number % 2 == 0)
+        printf("%d is even.", number);
+    else
+        printf("%d is odd.", number);
+
+    return 0;
+}
